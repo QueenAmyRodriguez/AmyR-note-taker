f8c201aca0cc594384ee4c28b567f8485dbf297a
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-17 16:14:30 -0700

    added post route

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json')
+const { notes } = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 
+});
+
+router.post('/notes', (req, res) => {
+    const note = req.body;
+
+    notes.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes }, null, 2)
+    );
+    return res.json(note);
+    
 });
 
 module.exports = router;
